2593fa59f75e10360970cad138bc8e101f90f073
ahmedbenseddiq <benseddiqahmed4@gmail.com>
2025-03-16 02:30:18 +0000

    model relations

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -6,8 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
+    protected $fillable = [
+        'user_id',
+    ];
+    
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
     }
 }
--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reservation extends Model
 {
-    
+    protected $fillable = [
+        'date_time',
+        'is_confirmed',
+        'is_canceled',
+        'is_done',
+        'client_id',
+        'chair_id',
+    ];
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function chair()
+    {
+        return $this->belongsTo(Chair::class);
+    }
 }
